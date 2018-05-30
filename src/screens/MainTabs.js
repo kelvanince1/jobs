cf2fb2677f587fe49b7c50728530ea3c6fd37663
Kelvan Ince <incekelvan@gmail.com>
2018-05-29 23:32:37 -0500

    Added the remaining screens to main tabs to be made available in navigation

--- src/screens/MainTabs.js
+++ src/screens/MainTabs.js
@@ -1,1 +1,21 @@
 import { Navigation } from 'react-native-navigation';
+
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      screen: 'jobs.WelcomeScreen',
+      label: 'Welcome',
+      title: 'Welcome'
+    },
+    {
+      screen: 'jobs.MapScreen',
+      label: 'Find your job',
+      title: 'Find your job'
+    },
+    {
+      screen: 'jobs.ReviewScreen',
+      label: 'Review',
+      title: 'Review'
+    }
+  ]
+})
