e261f8216c2b4d0f5e3ceecd644063898a4a14fb
Kelvan Ince <incekelvan@gmail.com>
2018-05-29 23:24:04 -0500

    Add the various boiler screens and tabs file

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class AuthScreen extends Component {
+  loginHandler = () => {
+
+  };
+  
   render() {
     return(
       <View>
         <Text>AuthScreen</Text>
+        <Button title="Log In" onPress={this.loginHandler} />
       </View>
     );
   };
---
+++ src/screens/ReviewScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ReviewScreen extends Component {
+  render() {
+    return (
+
+    );
+  };
+};
+
+export default ReviewScreen;
