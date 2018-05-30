1a0b568b2f103c5b7ccf8eb11ecc22428f426278
Kelvan Ince <incekelvan@gmail.com>
2018-05-29 23:35:50 -0500

    Use the main tabs navigation file to render screens after auth

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 
+import startMainTabs from './MainTabs';
+
 class AuthScreen extends Component {
   loginHandler = () => {
-
+    startMainTabs();
   };
-  
+
   render() {
     return(
       <View>
--- src/screens/MainTabs.js
+++ src/screens/MainTabs.js
@@ -1,21 +1,25 @@
 import { Navigation } from 'react-native-navigation';
 
-Navigation.startTabBasedApp({
-  tabs: [
-    {
-      screen: 'jobs.WelcomeScreen',
-      label: 'Welcome',
-      title: 'Welcome'
-    },
-    {
-      screen: 'jobs.MapScreen',
-      label: 'Find your job',
-      title: 'Find your job'
-    },
-    {
-      screen: 'jobs.ReviewScreen',
-      label: 'Review',
-      title: 'Review'
-    }
-  ]
-})
+const startTabs = () => {
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        screen: 'jobs.WelcomeScreen',
+        label: 'Welcome',
+        title: 'Welcome'
+      },
+      {
+        screen: 'jobs.MapScreen',
+        label: 'Find your job',
+        title: 'Find your job'
+      },
+      {
+        screen: 'jobs.ReviewScreen',
+        label: 'Review',
+        title: 'Review'
+      }
+    ]
+  })
+};
+
+export default startTabs;
