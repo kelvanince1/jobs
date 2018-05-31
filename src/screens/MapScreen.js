7f3ea6eaf883fd85733e4ae208eeb5bbc3744cca
Kelvan Ince <incekelvan@gmail.com>
2018-05-30 23:24:49 -0500

    Set lat long coordinates

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, StyleSheet} from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import MapView from 'react-native-maps';
 
 class MapScreen extends Component {
   state = {
     region: {
-      longitude: -122,
-      latitude: 37,
+      longitude: -122.431297,
+      latitude: 37.773972,
       longitudeDelta: 0.04,
       latitudeDelta: 0.09
     }
@@ -18,7 +18,13 @@ class MapScreen extends Component {
         <MapView
           initialRegion={this.state.region}
           style={styles.map}
+          onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <Button
+            large
+            title='Search this area'
+            backgroundColor='#009688'
+          />
       </View>
     );
   };
@@ -27,7 +33,7 @@ class MapScreen extends Component {
 const styles = StyleSheet.create({
   map: {
     width: "100%",
-    height: 250
+    height: "92%"
   }
 })
 
